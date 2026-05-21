30b2d3a0b7f9b8255391e935401e8a12776760b2
rivera <larmelendez3@gmail.com>
2026-05-21 08:27:49 -0600

    Flavor Slider component has been animated

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
 import NavBar from "./components/NavBar";
 import HeroSection from "./sections/HeroSection";
-import { ScrollTrigger } from "gsap/all";
+import { ScrollSmoother, ScrollTrigger } from "gsap/all";
 import gsap from "gsap";
 import MessageSection from "./sections/MessageSection";
 import FlavorSection from "./sections/FlavorSection";
+import { useEffect } from "react";
+import { useGSAP } from "@gsap/react";
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const App = () => {
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 3,
+      effects: true,
+    });
+  });
+
   return (
     <main>
       <NavBar />
-      <HeroSection />
-      <MessageSection />
-      <FlavorSection />
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <HeroSection />
+          <MessageSection />
+          <FlavorSection />
+        </div>
+      </div>
     </main>
   );
 };
