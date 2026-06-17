1caf7192e21fb1cd2b23e5568afb7e3e9b957aff
rivera <larmelendez3@gmail.com>
2026-06-17 17:42:53 -0600

    Animation in benefit section

--- src/sections/BenefitSection.jsx
+++ src/sections/BenefitSection.jsx
@@ -1,6 +1,46 @@
+import { useGSAP } from "@gsap/react";
 import ClipPathTitle from "../components/ClipPathTitle";
+import gsap from "gsap";
 
 const BenefitSection = () => {
+  useGSAP(() => {
+    const revealTl = gsap.timeline({
+      delay: 1,
+      scrollTrigger: {
+        trigger: ".benefit-section",
+        start: "top 60%",
+        end: "top top",
+        scrub: 1.5,
+      },
+    });
+
+    revealTl
+      .to(".benefit-section .first-title", {
+        duration: 1,
+        opacity: 1,
+        clipPath: "polygon(0% 0%, 100% 0, 100% 100%, 0% 100%)",
+        ease: "circ.out",
+      })
+      .to(".benefit-section .second-title", {
+        duration: 1,
+        opacity: 1,
+        clipPath: "polygon(0% 0%, 100% 0, 100% 100%, 0% 100%)",
+        ease: "circ.out",
+      })
+      .to(".benefit-section .third-title", {
+        duration: 1,
+        opacity: 1,
+        clipPath: "polygon(0% 0%, 100% 0, 100% 100%, 0% 100%)",
+        ease: "circ.out",
+      })
+      .to(".benefit-section .fourth-title", {
+        duration: 1,
+        opacity: 1,
+        clipPath: "polygon(0% 0%, 100% 0, 100% 100%, 0% 100%)",
+        ease: "circ.out",
+      });
+  });
+
   return (
     <section className="benefit-section">
       <div className="container mx-auto pt-20">
@@ -40,6 +80,10 @@ const BenefitSection = () => {
               className={"fourth-title"}
               borderColor={"#222123"}
             />
+          </div>
+
+          <div className="md:mt-0 mt-10">
+            <p>And much more ...</p>
           </div>
         </div>
       </div>
