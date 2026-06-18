e2c908c242031c9c5642d55031c7c38533313311
rivera <larmelendez3@gmail.com>
2026-06-17 19:09:49 -0600

    TestimonialSection with animation

--- src/sections/TestiomonialSection.jsx
+++ src/sections/TestiomonialSection.jsx
@@ -1,7 +1,58 @@
 import { useRef } from "react";
 import { cards } from "../constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 const TestimonialSection = () => {
   const vdRef = useRef([]);
+
+  useGSAP(() => {
+    gsap.set(".testimonials-section", {
+      marginTop: "-140vh",
+    });
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".testimonials-section",
+        start: "top bottom",
+        end: "200% top",
+        scrub: true,
+      },
+    });
+
+    tl.to(".testimonials-section .first-title", {
+      xPercent: 70,
+    })
+      .to(
+        ".testimonials-section .sec-title",
+        {
+          xPercent: 25,
+        },
+        "<",
+      )
+      .to(
+        ".testimonials-section .third-title",
+        {
+          xPercent: -50,
+        },
+        "<",
+      );
+
+    const pintl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".testimonials-section",
+        start: "10% top",
+        end: "200% top",
+        scrub: 1.5,
+        pin: true,
+      },
+    });
+
+    pintl.from(".vd-card", {
+      yPercent: 150,
+      stagger: 0.2,
+      ease: "power1.inOut",
+    });
+  });
 
   const handlePlay = (index) => {
     const video = vdRef.current[index];
